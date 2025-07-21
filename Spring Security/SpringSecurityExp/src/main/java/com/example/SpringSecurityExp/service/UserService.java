aa46ec1f0322da50b97e24bc202f716063a49b23
TAMILARASAN M <tamilarasanmuruganofficial@gmail.com>
2025-07-21 22:09:19 +0530

    Bcrypted the password and authenticated using the Bcrypted password

---
+++ Spring Security/SpringSecurityExp/src/main/java/com/example/SpringSecurityExp/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.SpringSecurityExp.service;
+
+import com.example.SpringSecurityExp.model.Users;
+import com.example.SpringSecurityExp.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    private BCryptPasswordEncoder encoder=new BCryptPasswordEncoder(12);
+
+    public Users register(Users user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepo.save(user);
+    }
+}
