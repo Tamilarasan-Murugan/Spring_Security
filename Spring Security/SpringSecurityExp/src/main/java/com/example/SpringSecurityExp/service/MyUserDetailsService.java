1e85988bb3006bb82fcbb834ac962b07b7b2330a
TAMILARASAN M <tamilarasanmuruganofficial@gmail.com>
2025-07-21 21:34:59 +0530

    Configured the DaoAuthentication provider using db

---
+++ Spring Security/SpringSecurityExp/src/main/java/com/example/SpringSecurityExp/service/MyUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.example.SpringSecurityExp.service;
+
+import com.example.SpringSecurityExp.model.UserPrincipal;
+import com.example.SpringSecurityExp.model.Users;
+import com.example.SpringSecurityExp.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepo.findByUsername(username);
+
+        if(user==null){
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return new UserPrincipal(user);
+    }
+}
