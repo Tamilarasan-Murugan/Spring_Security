afda4922b2e8640c06599c848f0d205df0fd56fe
TAMILARASAN M <tamilarasanmuruganofficial@gmail.com>
2025-07-22 20:59:13 +0530

    created the login and register API,Authenticated using the Authentication Manager

--- Spring Security/SpringSecurityExp/src/main/java/com/example/SpringSecurityExp/service/UserService.java
+++ Spring Security/SpringSecurityExp/src/main/java/com/example/SpringSecurityExp/service/UserService.java
@@ -3,6 +3,9 @@ package com.example.SpringSecurityExp.service;
 import com.example.SpringSecurityExp.model.Users;
 import com.example.SpringSecurityExp.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +15,22 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     private BCryptPasswordEncoder encoder=new BCryptPasswordEncoder(12);
 
     public Users register(Users user){
         user.setPassword(encoder.encode(user.getPassword()));
         return userRepo.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication authentication=authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
+
+        if(authentication.isAuthenticated()){
+            return "login success";
+        }
+        return "login failed";
     }
 }
